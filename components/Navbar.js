beb99b31145121a04b28d7c98f643892bcb99897
diegorepassa <diego.ferreira@repassa.com.br>
2022-02-24 12:49:35 -0300

    adicionado tooltip ao menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Avatar } from "@nextui-org/react";
+import { Avatar, Tooltip } from "@nextui-org/react";
 import { RiVipCrown2Fill as Crown } from "react-icons/ri";
 import { RiVipDiamondFill as Diamond } from "react-icons/ri";
 import { FaShoppingCart as Cart } from "react-icons/fa";
@@ -13,51 +13,97 @@ export default function Navbar() {
     <nav className="h-5/6 flex flex-col justify-between items-center w-36">
       <ul className="flex flex-col w-full justify-between items-center">
         <li>
-          <img src="/logo-2.png" alt="logo" className="px-8 transition hover:scale-110" />
+          <img
+            src="/logo-2.png"
+            alt="logo"
+            className="px-8 transition hover:scale-110"
+          />
         </li>
-        <li className="w-full flex flex-col justify-center items-center p-4 transition hover:scale-125">
-          <a href="#test">
-            <IconContext.Provider value={{ color: "white" }}>
-              <Crown size={25} fill="currentColor" />
-            </IconContext.Provider>
-          </a>
+        <li className="flex flex-col justify-center items-center p-4 transition hover:scale-125">
+          <Tooltip
+            color="primary"
+            content="Developers love Next.js"
+            placement="right"
+            enterDelay={0}
+          >
+            <a href="#test" className="">
+              <IconContext.Provider value={{ color: "white" }}>
+                <Crown size={25} fill="currentColor" />
+              </IconContext.Provider>
+            </a>
+          </Tooltip>
         </li>
-        <li className="w-full flex flex-col justify-center items-center p-4 transition hover:scale-125">
+        <li className="flex flex-col justify-center items-center p-4 transition hover:scale-125">
+        <Tooltip
+            color="primary"
+            content="Developers love Next.js"
+            placement="right"
+          >
           <a href="#">
             <IconContext.Provider value={{ color: "white" }}>
               <Diamond size={25} fill="currentColor" />
             </IconContext.Provider>
           </a>
+          </Tooltip>
         </li>
-        <li className="w-full flex flex-col justify-center items-center p-4 transition hover:scale-125">
+        <li className="flex flex-col justify-center items-center p-4 transition hover:scale-125">
+        <Tooltip
+            color="primary"
+            content="Developers love Next.js"
+            placement="right"
+          >
           <a href="#">
             <IconContext.Provider value={{ color: "white" }}>
               <Cart size={25} fill="currentColor" />
             </IconContext.Provider>
           </a>
+          </Tooltip>
         </li>
-        <li className="w-full flex flex-col justify-center items-center p-4 transition hover:scale-125">
+        <li className="flex flex-col justify-center items-center p-4 transition hover:scale-125">
+        <Tooltip
+            color="primary"
+            content="Developers love Next.js"
+            placement="right"
+          >
           <a href="#">
             <IconContext.Provider value={{ color: "white" }}>
               <Grid size={25} fill="currentColor" />
             </IconContext.Provider>
           </a>
+          </Tooltip>
         </li>
       </ul>
       <ul className="flex flex-col w-full justify-between items-center">
-        <li className="w-full flex flex-col justify-center items-center p-4">
-          <a href="#" className="transition hover:animate-spin">
+        <li className="flex flex-col justify-center items-center p-4">
+        <Tooltip
+            color="primary"
+            content="Developers love Next.js"
+            placement="right"
+          >
+          <a href="#" className="transition hover:scale-125">
             <IconContext.Provider value={{ color: "white" }}>
               <Settings size={25} fill="currentColor" />
             </IconContext.Provider>
           </a>
+          </Tooltip>
         </li>
-        <li className="w-full flex flex-col justify-center items-center p-4">
+        <li className="flex flex-col justify-center items-center p-4">
+        <Tooltip
+            color="primary"
+            content="Developers love Next.js"
+            placement="right"
+          >
           <a href="#">
             <IconContext.Provider value={{ color: "white" }}>
-              <Avatar size="lg" src="/avatars/avatar-default.png" color="error" bordered />
+              <Avatar
+                size="lg"
+                src="/avatars/avatar-default.png"
+                color="error"
+                bordered
+              />
             </IconContext.Provider>
           </a>
+          </Tooltip>
         </li>
       </ul>
     </nav>
